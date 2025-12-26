cccd3d938b17ee0d873e18138e803d7e025283e8
Jien Fei Yap <jyap@u80z04s05.42kl.edu.my>
2025-12-26 16:48:57 +0800

    Bug Fix

--- srcs/requirements/wordpress/tool/wp_setup.sh
+++ srcs/requirements/wordpress/tool/wp_setup.sh
@@ -1,45 +1,47 @@
 #!/bin/sh
 set -e
 
-#Ensure wordpress path exists
 mkdir -p /var/www/html
+cd /var/www/html
 
 DB_USER_PASSWORD=$(cat /run/secrets/db_user_password)
 WP_USER_PASSWORD=$(cat /run/secrets/wp_user_password)
 WP_ADMIN_PASSWORD=$(cat /run/secrets/wp_admin_password)
 
-echo "Waiting for mariadb..."
-until mariadb-admin --silent -h mariadb -u ${DB_USER} -p${DB_USER_PASSWORD} ping; do
+echo "Waiting for MariaDB..."
+until mariadb-admin --silent -h mariadb -u "$DB_USER" -p"$DB_USER_PASSWORD" ping; do
     sleep 1
 done
 
-#if wp-config does not exist
-if [ ! -f /var/www/html/wp-config.php ]; then
-    echo "Downloading Wordpress core..."
-    wp core download --path=/var/www/html --allow-root
+if [ ! -f wp-config.php ]; then
+    echo "Installing WordPress..."
 
-    echo "Creating wp-config.php..."
-    wp config create --path=/var/www/html --allow-root \
-        --dbname=${DB_NAME} \
-        --dbuser=${DB_USER} \
-        --dbpass=${DB_USER_PASSWORD} \
+    # Download only if core files are missing
+    if [ ! -f wp-load.php ]; then
+        wp core download --allow-root
+    fi
+
+    wp config create --allow-root \
+        --dbname="$DB_NAME" \
+        --dbuser="$DB_USER" \
+        --dbpass="$DB_USER_PASSWORD" \
         --dbhost=mariadb
 
-    echo "Installing Wordpress..."
-    wp core install --path=/var/www/html --allow-root \
-        --url=${WP_DOMAIN} \
-        --title=${WP_TITLE} \
-        --admin_user=${WP_ADMIN} \
-        --admin_password=${WP_ADMIN_PASSWORD} \
-        --admin_email=${WP_ADMIN_EMAIL}
+    wp core install --allow-root \
+        --url="$WP_DOMAIN" \
+        --title="$WP_TITLE" \
+        --admin_user="$WP_ADMIN" \
+        --admin_password="$WP_ADMIN_PASSWORD" \
+        --admin_email="$WP_ADMIN_EMAIL"
 
-    echo "Creating Wordpress User..."
-    wp user create --path=/var/www/html --allow-root \
-        ${WP_USER} ${WP_USER_EMAIL} \
-        --user_pass=${WP_USER_PASSWORD} \
+    wp user create --allow-root \
+        "$WP_USER" "$WP_USER_EMAIL" \
+        --user_pass="$WP_USER_PASSWORD" \
         --role=author
 
-    echo "Wordpress Installation Done."
+    echo "WordPress installation completed."
+else
+    echo "WordPress already installed. Skipping setup."
 fi
 
 echo "Starting PHP-FPM 8.3..."
